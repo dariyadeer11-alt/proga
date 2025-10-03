c75d331828f5ed3d0a7a979214542f2528d899a7
Darya <driyadeer11@gmail.com>
2025-10-03 18:43:39 +0300

    Zadanie_3

--- papka/main.py
+++ papka/main.py
@@ -3,3 +3,7 @@ print(kvadro)
 
 chet = [x for x in range(1,20) if x%2==0]
 print(chet)
+
+words = ['python', 'Java', 'c++', 'Rust', 'go']
+spisok = [x.upper() for x in words if len(x)>3]
+print(spisok)
