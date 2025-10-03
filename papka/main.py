e068af1fbb0a46419c19878ffa277f4a85f73e96
Darya <driyadeer11@gmail.com>
2025-10-03 19:02:59 +0300

    Zadanie_4

--- papka/main.py
+++ papka/main.py
@@ -7,3 +7,21 @@ print(chet)
 words = ['python', 'Java', 'c++', 'Rust', 'go']
 spisok = [x.upper() for x in words if len(x)>3]
 print(spisok)
+
+
+class Countdown:
+    def __init__(self, n):
+        self.n = n + 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.n -= 1
+        if self.n < 1:
+            raise StopIteration
+        return self.n
+
+
+for x in Countdown(5):
+    print(x)
